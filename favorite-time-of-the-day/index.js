9b7c5f9a4e3192d12b0cf762499da5b2b1c29fb9
codesyariah122 <pujiermanto@gmail.com>
2022-07-23 14:00:55 +0700

    favorite time of the day

---
+++ favorite-time-of-the-day/index.js
@@ -0,0 +1,36 @@
+// const express = require('express')
+
+// const path = require('path')
+
+// const dotenv = require('dotenv').config()
+import express from 'express'
+
+import path, {dirname} from 'path'
+
+import {fileURLToPath} from 'url'
+
+import {webRouter} from './router.js'
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+
+app.use(express.static('public/assets'))
+
+app.set('views', path.join(__dirname, 'public/pages'))
+
+app.set('view engine', 'ejs')
+
+webRouter(app)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ favorite-time-of-the-day/router.js
@@ -0,0 +1,10 @@
+export const webRouter = (app) => {
+	app.use('/', (req, res) => {
+		let context = {
+			"title": "Cassette Animation | codesyariah122",
+			"header_text": "Favorite time of the day",
+			"footer_text": "Made with &hearts; by codesyariah122" 
+		}
+		res.render('index', context)
+	})
+}
